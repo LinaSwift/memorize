af769a3d1809e9bff569d41d922d621c28ad6373
LinaK <gaklinskikh@gmail.com>
2025-06-20 15:02:18 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Galina Klinskikh on 20/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
